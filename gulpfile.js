76d0d561be7acd8828d78fd3679d89677941500b
Igor Sergiienko <Igor_Sergiienko@epam.com>
2016-06-24 16:33:23 +0300

    building finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,127 @@
 ﻿'use strict';
 
-var gulp = require('gulp');
-var rigger = require('gulp-rigger');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var spritesmith = require('gulp.spritesmith');
-var watch= require('gulp-watch');
-var open = require('gulp-open');
-var concat = require('gulp-concat');
-var clean = require('gulp-clean');
+const gulp = require('gulp');
+/* common plugins */
+const sourcemaps = require('gulp-sourcemaps');
+const watch= require('gulp-watch');
+const clean = require('gulp-clean');
+/* css plugins */
+const sass = require('gulp-sass');
+const prefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-minify-css');
+/* js plugins */
+const injectFile = require("gulp-inject-file");
+const jsmin = require('gulp-jsmin');
+const babel = require('gulp-babel');
+/* sprite and image plugins */
+const spritesmith = require('gulp.spritesmith');
+const imagemin = require('gulp-imagemin');
+/* server */
+const browserSync = require("browser-sync");
+const reload = browserSync.reload.bind(null, {stream: true});
 
-var path = {
-	src: {
-		html: 'src/index.html',
-		scss: ['src/styles/mixin.scss','src/styles/variable.scss','src/styles/*.scss', 'src/blocks/**/*.scss'],
-		sprites: 'src/sprites/*.png',
-		styles: 'src/styles/',
-		img: 'src/images/*.*',
-		root: 'src/'
+/* contants */
+const PATH = {
+	SRC: {
+		HTML: 'src/index.html',
+		SASS: 'src/main.sass',
+		JS: 'src/main.js',
+		SPRITES: 'src/sprites/*.png',
+		STYLES: 'src/styles/',
+		IMG: 'src/img/*.*',
+		ROOT: 'src/'
 	},
-	build: {
-		html: 'build/index.html',
-		img: 'build/images/',
-		root: 'build/'
-	},
-	watch: {
-		html: 'src/**/*.html'
-	},
+	BUILD: 'build/',
+	IMG: 'build/img/'
 };
 
-gulp.task('default', ['spr', 'html', 'css', 'img', 'watch'], () => gulp.start('open'));
+const SERVER_CONFIG = {
+	server: {
+		baseDir: PATH.BUILD
+	},
+	tunnel: true,
+	host: 'localhost',
+	port: 100,
+	logPrefix: 'Igor'
+};
 
-gulp.task('html', function () {
-	return gulp.src(path.src.html)
-			.pipe(rigger())
-			.pipe(gulp.dest(path.build.root));
-});
+const INJECT_CONFIG = { pattern: '// \\s*inject:<filename>' };
+const BABEL_CONFIG = { presets: ['es2015'] };
 
-gulp.task('css', () => {
-	return gulp.src(path.src.scss)
-			.pipe(concat('main.scss'))
+/* tasks */
+
+gulp.task('default', ['server', 'build', 'watch'] );
+
+gulp.task('server', () => browserSync(SERVER_CONFIG) );
+
+gulp.task('build', ['clean'], () => 
+	gulp.start( ['html', 'spr', 'img', 'js'] ) 
+);
+
+gulp.task('html', () => 
+		gulp
+			.src(PATH.SRC.HTML)
+			.pipe(gulp.dest(PATH.BUILD))
+			.pipe(reload())
+);
+
+gulp.task('js', () => 
+	gulp
+		.src(PATH.SRC.JS)
+		.pipe(injectFile(INJECT_CONFIG))
+		.pipe(sourcemaps.init())
+		.pipe(babel(BABEL_CONFIG))
+		.pipe(jsmin())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(PATH.BUILD))
+		.pipe(reload())
+);
+
+gulp.task('css', () => 
+		gulp
+			.src(PATH.SRC.SASS)
+			.pipe(sourcemaps.init())
 			.pipe(sass().on('error', sass.logError))
-			.pipe(postcss([autoprefixer({
-				browsers: 'last 2 versions, ie 8-11'
-			})]))
-			.pipe(gulp.dest(path.build.root));
-});
+			.pipe(prefixer())
+			.pipe(cssmin())
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest(PATH.BUILD))
+			.pipe(reload())
+);
+
+gulp.task('img', () =>
+		gulp
+			.src(PATH.SRC.IMG)
+			.pipe(imagemin())
+			.pipe(gulp.dest(PATH.IMG))
+);
 
 gulp.task('spr', () => {
-	var spriteData = 
-		gulp.src(path.src.sprites) 
-			.pipe(spritesmith({
-				imgName: 'sprite.png',
-				cssName: 'sprite.scss',
-				cssFormat: 'scss',
-				algorithm: 'top-down',
-				padding: 2,
-				cssVarMap: function (sprite) {sprite.name = 'sprite_' + sprite.name;}
-			}));
-	spriteData.img.pipe(gulp.dest(path.build.root)); 
-	spriteData.css.pipe(gulp.dest(path.src.styles)); 
+	var spriteData = gulp
+						.src(PATH.SRC.SPRITES) 
+						.pipe(spritesmith({
+							imgName: 'sprite.png',
+							cssName: 'sprite.sass',
+							cssFormat: 'sass',
+							algorithm: 'top-down',
+							padding: 2,
+							cssVarMap: function (sprite) {sprite.name = 'sprite_' + sprite.name;}
+						}));
+	spriteData.img.pipe(gulp.dest(PATH.IMG)); 
+	spriteData.css.pipe(gulp.dest(PATH.SRC.STYLES)); 
 	gulp.start('css');
 	return spriteData;
 });
 
-gulp.task('img', () => {
-	return gulp.src(path.src.img)
-			.pipe(gulp.dest(path.build.img));
-});
-
 gulp.task('watch', () => {
-	watch([path.watch.html], function(event, cb) {
-		gulp.start('html');
-	});
-	watch(path.src.scss, function(event, cb) {
-		gulp.start('css');
-	});
-	watch([path.src.sprites], function(event, cb) {
-		gulp.start('spr');
-	});
-	watch([path.src.img], function(event, cb) {
-		gulp.start('img');
-	});
+	watch([PATH.SRC.HTML], () => gulp.start('html') );
+	watch([PATH.SRC.SASS], () => gulp.start('css') );
+	watch([PATH.SRC.SPRITES], () => gulp.start('spr') );
+	watch([PATH.SRC.IMG], () => gulp.start('img') );
 });
 
-gulp.task('open', () => {
-	var options = {
-		app: 'chrome',
-		uri: path.build.html
-	};
-	return gulp.src('').pipe(open(options));
-});
-
-gulp.task('clean', function () {
-	return gulp.src(path.build.root, {read: false})
-		.pipe(clean());
-});
+gulp.task('clean',  () =>
+	gulp
+		.src(PATH.BUILD, {read: false})
+		.pipe(clean())
+);
---
+++ src/main.js
@@ -0,0 +1,8 @@
+﻿(function(){
+	'use strict';
+	// inject:'bower_components/angular/angular.min.js'
+	// inject:'bower_components/angular-ui-router/release/angular-ui-router.min.js'
+	// inject: main_js/app.js 
+	// inject: main_js/helpers.js
+
+})();
